14b0c990fd3b1bc2738682918b6152cce16dbcff
Lefteris Nikolidakis <tab114@gmail.com>
2020-11-20 02:04:25 +0200

    deployed till error boundaries

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,16 +1,42 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Results from "./Results";
 
 const SearchParams = () => {
 	const [location, setLocation] = useState("Seattle, WA");
 	const [breeds, setBreeds] = useState([]);
 	const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-	const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+	const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+	const [pets, setPets] = useState([]);
+
+	async function requestPets() {
+		const { animals } = await pet.animals({
+			location,
+			breed,
+			type: animal,
+		});
+
+		setPets(animals || []);
+	}
+
+	useEffect(() => {
+		setBreeds([]);
+		setBreed("");
+		pet.breeds(animal).then(({ breeds }) => {
+			const breedsString = breeds.map(({ name }) => name);
+			setBreeds(breedsString);
+		}, console.error);
+	}, [animal, setBreed, setBreeds]);
 
 	return (
 		<div className="search-params">
-			<form>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					requestPets();
+				}}
+			>
 				<label htmlFor="location">Location</label>
 				<input
 					id="location"
@@ -22,6 +48,7 @@ const SearchParams = () => {
 				<BreedDropdown />
 				<button>Submit</button>
 			</form>
+			<Results pets={pets} />
 		</div>
 	);
 };
